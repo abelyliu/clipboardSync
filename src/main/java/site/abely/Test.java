eae93dfc6dc3970110fddc3b61406f657e0d27e5
刘青松 <qingqong.liu@crgecent.com>
2020-03-22 11:56:56 +0800

    udp广播测试

---
+++ src/main/java/site/abely/Test.java
@@ -0,0 +1,27 @@
+package site.abely;
+
+import java.io.IOException;
+import java.net.*;
+
+public class Test {
+    public static void main(String[] args) {
+        // 广播的实现 :由客户端发出广播，服务器端接收
+        String host = "255.255.255.255";//广播地址
+        int port = 9999;//广播的目的端口
+        String message = "this is clip sync tool";//用于发送的字符串
+        try {
+            InetAddress adds = InetAddress.getByName(host);
+            DatagramSocket ds = new DatagramSocket();
+            DatagramPacket dp = new DatagramPacket(message.getBytes(),
+                    message.length(), adds, port);
+            ds.send(dp);
+            ds.close();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
